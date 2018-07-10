003d561484c84a40399f16a39e111432cc4d7512
qiuzhilai <qiuzhilai@luochen.com>
2018-07-10 19:38:38 +0800

    1、where语法

---
+++ linq/linqConsole/01-Restriction Operators/Where-Simple1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linqConsole
+{
+    public class Where_Simple1
+    {
+        int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        /// <summary>
+        /// 点标记
+        /// </summary>
+        public void Show()
+        {
+            Console.WriteLine("Where_Simple1 点标记");
+            //找出数组中小于5的集合并打印
+            var result = numbers.Where(t => t < 5);
+            foreach (var item in result)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// 查询表达式
+        /// </summary>
+        /// <param name="x"></param>
+        public void Show(int x)
+        {
+            Console.WriteLine("Where_Simple1 查询表达式");
+            //找出数组中小于5的集合并打印
+            var result = from t in numbers
+                       where t < 5
+                       select t;
+            foreach (var item in result)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    
+
+    
+}
---
+++ linq/linqConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace linqConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Where_Simple1
+            Where_Simple1 simple1 = new Where_Simple1();
+            simple1.Show();
+            simple1.Show(0);
+            #endregion
+
+            #region Where_Simple2
+            Where_Simple2 simple2 = new Where_Simple2();
+            simple2.Show();
+            simple2.Show(0);
+            #endregion
+
+            Console.Read();
+        }
+        
+    }
+
+
+}
